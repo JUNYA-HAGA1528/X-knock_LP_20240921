93b5dea81b475666b00d68b3ace472022543f161
Haga Junya <n0812034@yahoo.co.jp>
2024-09-23 02:41:29 +0900

    2024/9/23

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+    window.addEventListener('scroll', function() {
+        const header = document.querySelector('.header');
+        if (window.scrollY > 300) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+    
+    
 
 });
