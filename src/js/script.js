76a7fa8adafd72b517d55f4d7895cf088afdc0d6
Haga Junya <n0812034@yahoo.co.jp>
2024-09-23 14:56:31 +0900

    2024/9/23

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,21 @@ jQuery(document).ready(function ($) {
         event.preventDefault(); // リンクのデフォルト動作を無効化
         $('html, body').animate({ scrollTop: 0 }, 1000); // スムーズにトップへスクロール（500ms）
     });
+
+
+
+    document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+        anchor.addEventListener('click', function(event) {
+            event.preventDefault();  // デフォルトのページ内リンク動作を無効化
+            const targetId = this.getAttribute('href'); // クリックされたリンクのhref属性を取得
+            const target = document.querySelector(targetId); // 取得したIDからターゲット要素を選択
+    
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop - 80,  // スクロール位置を50px分調整
+                    behavior: 'smooth'           // スムーズなスクロール動作
+                });
+            }
+        });
+    });
 });
